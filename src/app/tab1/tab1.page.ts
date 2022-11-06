5f67cc1a0da76c943a96dd2cb8392358999e6dba
Dragomir Jovanovic <dj20180232@student.fon.bg.ac.rs>
2022-11-06 03:24:22 +0100

    Izmene na tab1 i tab2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  credentials: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authService: UserService,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private router: Router
+  ) { }
 
-  constructor() {}
+  get email(){
+    return this.credentials.get('email');
+  }
 
+  get password(){
+    return this.credentials.get('password');
+  }
+
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(7)]]
+    });
+  }
+
+  async _register(){
+    const loading = await this.loadingController.create();
+		await loading.present(); //Loading 
+
+		const _user = await this.authService.__register(this.credentials.value);
+		await loading.dismiss(); //Remove loading when done!
+
+		if (_user) {
+			this.router.navigateByUrl('/tabs/tab2', { replaceUrl: true });
+		} else {
+			this.showAlert('Neuspela registracija', 'Molimo Vas pokusajte ponovo!');
+		}
+  }
+
+  async _login(){
+    const loading = await this.loadingController.create();
+		await loading.present();  //Loading 
+
+		const _user = await this.authService.__login(this.credentials.value);
+		await loading.dismiss(); //Remove loading when done!
+
+		if (_user) {
+			this.router.navigateByUrl('/tabs/tab2', { replaceUrl: true });
+		} else {
+			this.showAlert('Neuspesna prijava', 'Molimo Vas pokusajte ponovo!');
+		}
+  }
+
+  async showAlert(header, message){
+    const _alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await _alert.present();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddNewItemPage } from '../add-new-item/add-new-item.page';
+import { UpdateItemPage } from '../update-item/update-item.page';
+import { DataService } from '../service/data.service';
+import { Subscription } from 'rxjs';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 }) /* DJ DJ DJ - TAB2 */
-export class Tab2Page {
-  constructor() {}
+export class Tab2Page implements OnInit, OnDestroy {
+  sub: Subscription;
+  dj: any;
+  constructor(public modalCtrl: ModalController, private dataService: DataService, private authService: UserService) {}
+  ngOnInit() {
+    this.__getData();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  async __getData(){ /* Get FIREBASE/FIRESTORE data! */
+    this.sub = await this.dataService.__getData().subscribe((__res) => {
+      this.dj = __res; //console.log(this.dj);
+    })
+  }
+
+  async __goToAddPage() {
+    const _modal = await this.modalCtrl.create({
+      component: AddNewItemPage,
+    });
+    return await _modal.present();
+  }
+
+  async __goToUpdatePage(dj) {
+    const modal = await this.modalCtrl.create({
+      component: UpdateItemPage,
+      componentProps: { dj },
+    });
+    return await modal.present();
+  }
+
+  __logout(){
+    return this.authService.__logout();
+  }
+  //DELETE IS IN UPDATE-ITEM!
 }
